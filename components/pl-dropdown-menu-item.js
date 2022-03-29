90ff27a3cd120a549d92300345a93b182da7d20c
Azat Yamalov <yamalov.azat@live.com>
2022-03-29 22:10:02 +0300

    simple dropdown-menu

---
+++ components/pl-dropdown-menu-item.js
@@ -0,0 +1,58 @@
+import { PlElement, html, css } from "polylib";
+import '@plcmp/pl-icon';
+
+class PlDropdownMenuItem extends PlElement {
+    static get properties() {
+        return {
+            label: {
+                type: String
+            },
+            disabled: {
+                type: Boolean,
+                reflectToAttribute: true
+            }
+        };
+    }
+
+    static get css() {
+        return css`
+            :host{
+                box-sizing: border-box;
+                padding: 0 var(--space-sm);
+                min-height: var(--base-size-md);
+                width: 100%;
+                font: var(--text-font);
+                color: var(--text-color);
+                display: flex;
+                align-items: center;
+                cursor: pointer;
+            }
+            :host(:hover){
+                background-color: var(--grey-lightest)
+            }
+            :host([disabled]){
+                color: var(--grey-base);
+                pointer-events: none;
+            }
+            :host([hidden]){
+                display: none;
+            }
+        `;
+    }
+
+    static get template() {
+        return html`
+            [[label]]
+            <slot></slot>
+            `;
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.addEventListener('click', (e) => {
+            this.parentElement.close();
+        })
+    }
+}
+
+customElements.define('pl-dropdown-menu-item', PlDropdownMenuItem);
---
+++ components/pl-dropdown-menu.js
@@ -0,0 +1,49 @@
+import { PlElement, html, css } from "polylib";
+import { PlDropdown } from '@plcmp/pl-dropdown';
+
+class PlDropdownMenu extends PlElement {
+    static get properties() {
+        return {
+            /**
+             * Является ли этот компонент элементом подменю.
+             * @memberOf NfDropdownMenu
+             * @private
+             */
+            _isSubMenu: {
+                type: Boolean
+            }
+        };
+    }
+
+    static get css() {
+        return css`
+            pl-dropdown[opened] {
+                display: flex;
+                flex-direction: column;
+                background: var(--surface-color);
+                border-radius: var(--border-radius);
+                box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.08);
+                min-width: var(--content-width);
+                box-sizing: border-box;
+                overflow: auto;
+                padding: var(--space-md) 0;
+            }`
+    }
+
+    static get template() {
+        return html`
+            <pl-dropdown id="dd">
+                <slot></slot>
+            </pl-dropdown>
+        `;
+    }
+
+    open(target, fitInto) {
+        this.$.dd.open(target, fitInto);
+    }
+
+    close(){
+        this.$.dd.close();
+    }
+}
+customElements.define('pl-dropdown-menu', PlDropdownMenu);
