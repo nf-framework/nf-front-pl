7f2aacf8b47df3c2bc288badd741127e4941b0e9
Denis Parfenov <denis.parfenov0@gmail.com>
2022-06-04 14:27:19 +0300

    Исправление pl-menu-form, правка верстки

--- components/pl-menu-form-item.js
+++ components/pl-menu-form-item.js
@@ -40,7 +40,6 @@ class PlMenuFormItem extends PlElement {
         return css`
             :host{
                 width: 100%;
-                height: 100%;
                 box-sizing: border-box;
                 flex-shrink: 0;
             }
