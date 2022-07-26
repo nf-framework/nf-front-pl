b71d33e2cc18018173bdd448deba0107daf6c6c9
Dmitrii Boikov <boikovdv@gmail.com>
2022-07-26 13:28:16 +0300

    fix wrong vertical size of form container

--- components/pl-forms-thread.js
+++ components/pl-forms-thread.js
@@ -18,6 +18,7 @@ class FormThreadManager extends PlElement {
           display: block;
           height: 100%;
           width: 100%;
+          contain: size;
         }
         :host([hidden]) {
           display: none;
