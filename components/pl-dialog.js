f7f45da429f5ac0566560367389eb9a05c0d2618
Azat Yamalov <yamalov.azat@live.com>
2023-09-18 11:59:03 +0300

    dialogs position

--- components/pl-dialog.js
+++ components/pl-dialog.js
@@ -13,7 +13,7 @@ class PlAppMenu extends PlElement {
         :host {
             background: rgba(36, 51, 49, 0.4);
             height: 100%;
-            position: fixed;
+            position: var(--pl-modal-position, fixed);
             top: 0;
             right: 0;
             bottom: 0;
